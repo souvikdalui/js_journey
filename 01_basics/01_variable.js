c59b364768e850beb119beb7334bf739e36a864a
Souvik Dalui <souvikdalui9@gmail.com>
2023-08-20 10:41:54 +0000

    check

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,17 @@
+// syntax of variable
+
+const accountId = 12345; // "const" - once declared, we can't change its value.
+let accountEmail = "souvik@gmail.com"; // "let" - we can change its value whenever we need.
+var accountPassword = "234521" // old syntax, there are issues with scoping, which is not a good practice. 
+ accountCity = "kolkata"
+let accountState ;
+
+
+console.log(accountId);
+
+// 
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+
+//
